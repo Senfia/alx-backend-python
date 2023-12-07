c8862673469430dee8ef007917e9360f603f5361
Senyo Fiawornu <contactsenyo@gmail.com>
2023-12-07 20:35:01 +0000

    finished final task

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,15 @@
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''This function generates several duplicates of the elements in a tuple.
+    '''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
