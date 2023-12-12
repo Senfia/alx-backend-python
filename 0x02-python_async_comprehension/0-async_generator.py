6d00043bbc9d09732473844df04fc9ea0d42c1dc
Senyo Fiawornu <contactsenyo@gmail.com>
2023-12-12 19:39:58 +0000

    initial commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Async Generator Module
+'''
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.Generator[float, None, None]:
+    '''yield a random number between 0 and 10 after 10 loops
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
